ef98940e6e1180e26593c5296ce5bed0b6c5920f
NATHAN SOARES GONÇALVES <2025308214@MACEIO.IFAL>
2026-09-01 15:07:03 -0300

    desisto

--- src/rotas/links.js
+++ src/rotas/links.js
@@ -9,27 +9,24 @@ let proximoId = 1;
 // GET /links — lista todos os links.
 router.get("/links", (req, res) => {
   // TODO (Tarefa B): responda com status 200 e o array `links`.
-  res.status(200).send(links);
+  res.status(201).json(links);
 });
 
 // POST /links — cria um link { titulo, url } (ambos TEXTO/string).
 router.post("/links", (req, res) => {
-  let id = new Date().getTime();
+  // 1. Leia titulo (texto) e url (texto) de req.body.
   const { titulo, url } = req.body;
 
+  // 2. Se faltar titulo OU url, responda 400.
   if (titulo === undefined || url === undefined) {
-    res.status(400).send("falta titulo ou url");
-  } else {
-    let linkCriado = { id: id, titulo, url };
-    links.push(linkCriado);
-    res.status(201).json(linkCriado);
+    return res.status(400).send("falta titulo ou url");
   }
 
-  // TODO (Tarefa B):
-  // 1. Leia titulo (texto) e url (texto) de req.body.
-  // 2. Se faltar titulo OU url, responda 400.
-  // 3. Crie { id: proximoId++, titulo, url }, adicione em `links`
-  // e responda 201 com o link criado.
+  // 3. Crie { id: proximoId++, titulo, url }, adicione em `links` e responda 201 com o link criado.
+  const linkCriado = { id: proximoId++, titulo, url };
+  links.push(linkCriado);
+  
+  res.status(201).json(linkCriado);
 });
 
 module.exports = router;
