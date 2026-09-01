3463dee29733d6ae67eb0d6cd06b9866164b1ac2
NATHAN SOARES GONÇALVES <2025308214@MACEIO.IFAL>
2026-09-01 14:32:35 -0300

    link

--- src/rotas/links.js
+++ src/rotas/links.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const router = express.Router();
 
 // ─── Tarefa B — Links úteis ───────────────────────────────────────────────────
@@ -8,19 +7,29 @@ const links = [];
 let proximoId = 1;
 
 // GET /links — lista todos os links.
-router.get("/", (req, res) => {
+router.get("/links", (req, res) => {
   // TODO (Tarefa B): responda com status 200 e o array `links`.
-  res.status(501).json({ erro: "não implementado" });
+  res.status(200).send(links);
 });
 
 // POST /links — cria um link { titulo, url } (ambos TEXTO/string).
-router.post("/", (req, res) => {
+router.post("/links", (req, res) => {
+  let id = new Date().getTime();
+  const { titulo, url } = req.body;
+
+  if (titulo === undefined || url === undefined) {
+    res.status(400).send("falta titulo ou url");
+  } else {
+    let linkCriado = { id: id, titulo, url };
+    links.push(linkCriado);
+    res.status(201).json(linkCriado);
+  }
+
   // TODO (Tarefa B):
-  //  1. Leia titulo (texto) e url (texto) de req.body.
-  //  2. Se faltar titulo OU url, responda 400.
-  //  3. Crie { id: proximoId++, titulo, url }, adicione em `links`
-  //     e responda 201 com o link criado.
-  res.status(501).json({ erro: "não implementado" });
+  // 1. Leia titulo (texto) e url (texto) de req.body.
+  // 2. Se faltar titulo OU url, responda 400.
+  // 3. Crie { id: proximoId++, titulo, url }, adicione em `links`
+  // e responda 201 com o link criado.
 });
 
 module.exports = router;
